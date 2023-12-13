25a8710811330697df5ca81d3ce4aedd016a2906
nagawaRyosuke <74583868+nagawaRyosuke@users.noreply.github.com>
2023-12-13 14:16:40 +0900

    Update cypress.config.cjs

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -4,18 +4,19 @@ const webpackPreprocessor = require('@cypress/webpack-preprocessor')
 const { removeDirectory } = require('cypress-delete-downloads-folder');
 
 module.exports = defineConfig({
-  pluginsFile: false,
-  modifyObstructiveCode: false,
-  experimentalStudio: true,
-  reporter: "mochawesome",
-  reporterOptions: {
-    reportDir: "cypress",
-    overwrite: false,
-    html: false,
-    json: true
+  // pluginsFile: false,
+  // modifyObstructiveCode: false,
+  // experimentalStudio: true,
+  reporter: 'cypress-mochawesome-reporter',
+  // reporter: "mochawesome",
+  // reporterOptions: {
+  //   reportDir: "cypress",
+  //   overwrite: false,
+  //   html: false,
+  //   json: true
   },
-  "screenshotsFolder": "cypress/screenshots",
-  "videosFolder": "cypress/videos",
+  // "screenshotsFolder": "cypress/screenshots",
+  // "videosFolder": "cypress/videos",
   requestTimeout: 10000,
   defaultCommandTimeout: 10000,
   component: {
@@ -29,6 +30,16 @@ module.exports = defineConfig({
     numTestsKeptInMemory: 50,
     baseUrl: `http://localhost:8089`,
     setupNodeEvents(on, config) {
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        await beforeRunHook(details);
+      });
+
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
+      require('cypress-mochawesome-reporter/plugin')(on);
       on('task', {
         removeDirectory,
         log(message) {
