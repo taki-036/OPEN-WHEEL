b6c15f9bb9fbe67dce678f92cdc951c068f02b36
nagawaRyosuke <74583868+nagawaRyosuke@users.noreply.github.com>
2023-12-14 17:56:32 +0900

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,16 +16,16 @@
  * @type {Cypress.PluginConfig}
  */
 
-import addContext from 'mochawesome/addContext'
+// import addContext from 'mochawesome/addContext'
 
-Cypress.on('test:after:run', (test, runnable) => {
-  if (test.state === 'failed') {
-    addContext(
-      { test },
-      `./screenshots/${location.pathname.replace(/(.*)\//, '')}/${runnable.parent.title} -- ${
-        test.title
-      } (failed).png`
-    )
-  }
-  addContext({ test }, `./videos/${location.pathname.replace(/(.*)\//, '')}.mp4`)
-})
+// Cypress.on('test:after:run', (test, runnable) => {
+//   if (test.state === 'failed') {
+//     addContext(
+//       { test },
+//       `./screenshots/${location.pathname.replace(/(.*)\//, '')}/${runnable.parent.title} -- ${
+//         test.title
+//       } (failed).png`
+//     )
+//   }
+//   addContext({ test }, `./videos/${location.pathname.replace(/(.*)\//, '')}.mp4`)
+// })
