8d9d872d7b6f8b5f12f02f97939581a7c42e0769
nagawaRyosuke <74583868+nagawaRyosuke@users.noreply.github.com>
2023-12-19 18:40:48 +0900

    Update cypress.config.cjs

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -4,8 +4,7 @@ const webpackPreprocessor = require('@cypress/webpack-preprocessor')
 const { removeDirectory } = require('cypress-delete-downloads-folder');
 
 module.exports = defineConfig({
-  projectId: "4jprfr"
-  },
+  projectId: "4jprfr",
   requestTimeout: 50000,
   defaultCommandTimeout: 50000,
   component: {
@@ -16,14 +15,14 @@ module.exports = defineConfig({
   },
 
   e2e: {
-    numTestsKeptInMemory: 1,
-    baseUrl: `http://localhost:8089`,
-    screenshotsFolder: "screenshots",
     env: {
       CYPRESS_RECORD_KEY: "c9eabe66-818d-470f-a4a8-5c33b028f1b3",
       browserPermissions: {
         clipboard: "allow"
-      },
+      }
+    },
+    numTestsKeptInMemory: 1,
+    baseUrl: `http://localhost:8089`,
     setupNodeEvents(on, config) {
       on('task', {
         removeDirectory,
