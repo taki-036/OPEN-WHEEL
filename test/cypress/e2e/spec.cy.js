792c92c7f00c7cf6615a9f91f852b3ed5616ae92
nagawaRyosuke <74583868+nagawaRyosuke@users.noreply.github.com>
2024-01-22 13:47:08 +0900

    Update spec.cy.js

--- test/cypress/e2e/spec.cy.js
+++ test/cypress/e2e/spec.cy.js
@@ -575,7 +575,7 @@ describe('wheel test', () => {
     cy.removeTask('task0')
   })
 
-  it('test27', () => {
+  it.only('test27', () => {
     cy.taskMake('task0')
     cy.scriptMake('run.sh', 'echo test')
     cy.openScriptSelectBox()
