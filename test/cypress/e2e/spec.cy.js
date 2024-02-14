7d4c351d7702f16b2606fb31619cd5179f1862b4
nagawaRyosuke <74583868+nagawaRyosuke@users.noreply.github.com>
2024-02-14 19:30:19 +0900

    Update spec.cy.js

--- test/cypress/e2e/spec.cy.js
+++ test/cypress/e2e/spec.cy.js
@@ -1075,7 +1075,7 @@ describe('wheel test', () => {
     cy.removeTask('task0')
   })
   
-  it.only('test41', () => {
+  it('test41', () => {
     cy.taskMake('task0')
     cy.hostSelect(remotehost)
     cy.scriptMake('run.sh', 'echo test')
